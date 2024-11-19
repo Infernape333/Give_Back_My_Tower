a51c6dc0e48c3f2e9c9bdcc8a3bf7f492aab2095
Infernape333 <cauameloponte9595@gmail.com>
2024-11-19 20:23:58 -0300

    GUIcompleto

--- Give_Back_My_Tower!-20241110T010901Z-001/Give_Back_My_Tower!/Scripts/Control_inicio.gd
+++ Give_Back_My_Tower!-20241110T010901Z-001/Give_Back_My_Tower!/Scripts/Control_inicio.gd
@@ -1,19 +1,22 @@
 extends Control
+@onready var audio = $"../AudioStreamPlayer"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	audio.play()
 
 
-@onready var btn1 = $start
-@onready var btn2 = $quit
-
-func _unhandled_input(event):
-	if event.is_action_pressed("ui_up"):
-		btn1.grab_focus()
-	elif event.is_action_pressed("ui_down"):
-		btn2.grab_focus()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://Scenes/inicio.tscn")
+	TransitionManager.fade_to_scene("res://Scenes/inicio.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
 
+
+func _on_audio_stream_player_finished():
+	audio.play()

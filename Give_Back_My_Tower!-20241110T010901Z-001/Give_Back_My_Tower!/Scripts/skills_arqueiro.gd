a47cf83e20dade890fad997b22af76beaaf3e8b7
Hélio Nogueira <heliofilhofhnlf@gmail.com>
2024-11-19 14:34:17 -0300

    arqueiro

--- Give_Back_My_Tower!-20241110T010901Z-001/Give_Back_My_Tower!/Scripts/skills_arqueiro.gd
+++ Give_Back_My_Tower!-20241110T010901Z-001/Give_Back_My_Tower!/Scripts/skills_arqueiro.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 var speed: float = 200
-var MagicType: String = "FireBolt"
+var MagicType: String = "ataque_1"
+@onready var Exp_collision = $ExpDmg
 
 var direction: Vector2 = Vector2.RIGHT:
 
@@ -9,24 +10,38 @@ var direction: Vector2 = Vector2.RIGHT:
 		direction = value
 		rotation = direction.angle()
 
+
 func _physics_process(delta):
 	position += speed * direction * delta
 	 
-func play(animation_name = "FireBolt"):
+func play(animation_name = "ataque_1"):
 	$AnimatedSprite2D.play(animation_name)
-	
-func _on_visible_on_screen_enabler_2d_screen_exited():
-	queue_free()
-
 
 func _on_body_entered(body):
 	if body.is_in_group("enemies"):
-		if $AnimatedSprite2D.animation == "FireBolt":
+		if $AnimatedSprite2D.animation == "ataque_1":
+			explosion()
 			body.hurtFire()
-		elif $AnimatedSprite2D.animation == "IceSpikes":
+		elif $AnimatedSprite2D.animation == "ataque_2":
 			body.hurtIce()
-		elif $AnimatedSprite2D.animation == "DarkSkull":
+			queue_free()
+		elif $AnimatedSprite2D.animation == "ataque_3":
 			body.hurtDark()
+			queue_free()
 		else:
 			body.hurt()
-		queue_free()
+		
+		$CollisionShape2D.disabled = true
+		speed = 0
+
+func explosion():
+	$AnimatedSprite2D.play("FireBoltExp")
+	$CollisionShape2D.disabled = true
+	await $AnimatedSprite2D.animation_finished
+	queue_free()
+	$CollisionShape2D.disabled = false
+
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()

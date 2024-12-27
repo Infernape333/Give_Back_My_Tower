78f2b1a32b0b1a5abbbe8938679a21fc143ae896
Infernape333 <cauameloponte9595@gmail.com>
2024-12-27 19:32:43 -0300

    additens

--- Give_Back_My_Tower!-20241110T010901Z-001/Give_Back_My_Tower!/floor_1.gd
+++ Give_Back_My_Tower!-20241110T010901Z-001/Give_Back_My_Tower!/floor_1.gd
@@ -6,6 +6,7 @@ var PillarBuffs = preload("res://Scenes/pillar_buffs.tscn")
 @onready var target = $Direction
 @onready var porta = $door
 @onready var Buff1 = $Marker2D
+@onready var Buff2 = $Marker2D2
 var buff_spawned = false 
 
 
@@ -26,8 +27,11 @@ func _on_timer_timeout():
 func Spawn_Buff1():
 	if not buff_spawned:  
 		var buff_instantiate = PillarBuffs.instantiate()
+		var buff_instantiate2 = PillarBuffs.instantiate()
 		buff_instantiate.global_position = Buff1.global_position
+		buff_instantiate2.global_position = Buff2.global_position
 		add_child(buff_instantiate)
+		add_child(buff_instantiate2)
 		buff_spawned = true  # Atualiza o flag para evitar novos spawns
 #func _on_cursor_timer_timeout():
 	#cursor.visible = true

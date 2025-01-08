508c2cc3c5c9470367783ab58b6ed270664bd13f
jeancsanchez <jeancs.patricio@gmail.com>
2025-01-08 11:11:13 -0300

    Refactoring to show aquired label when player buy one item

--- Give_Back_My_Tower!-20241110T010901Z-001/Give_Back_My_Tower!/Scripts/PlayerBase.gd
+++ Give_Back_My_Tower!-20241110T010901Z-001/Give_Back_My_Tower!/Scripts/PlayerBase.gd
@@ -5,17 +5,20 @@ const MAX_LIFE = 100
 const _INITIAL_ATTACK = 5
 const _INITIAL_DEFENSE = 0
 const _DEFAULT_HEAL = 10
+const _INITIAL_COINS = 5
 
 # Do not change this variables directly
 var _attack = _INITIAL_ATTACK
 var _defense = _INITIAL_DEFENSE
 var _current_life: int = MAX_LIFE
+var _curr_coins: int = _INITIAL_COINS
 var _inventory_items : Array[InventoryItem] = []
 
 func add_inventory_item(item: InventoryItem):
 	if _is_item_already_added(item): return	
 	_attack += item.get_attack()
 	_defense += item.get_defense()
+	_curr_coins -= item.get_coins()
 	_inventory_items.push_back(item)
 
 func set_life_damage(damage: int):
@@ -28,6 +31,17 @@ func heal(amount: int = _DEFAULT_HEAL):
 	
 func has_inventory_item(item: InventoryItem):
 	return _is_item_already_added(item)
+	
+func get_coins() -> int:
+	return _curr_coins
+
+func set_coins(amount: int):
+	_curr_coins = amount
+	
+func can_buy_inventory_item(item: InventoryItem) -> bool:
+	if get_coins() >= item.get_coins():
+		return true
+	return false
 	
 func reset_states():
 	_current_life = MAX_LIFE

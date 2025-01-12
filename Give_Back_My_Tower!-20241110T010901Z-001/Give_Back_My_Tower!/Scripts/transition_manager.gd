31a2f4e761e7e0aeda32c92af4d9c830c9a5030c
Jean Patricio <jeancs.patricio@gmail.com>
2025-01-12 04:00:41 -0300

    Fixing game over player state

--- Give_Back_My_Tower!-20241110T010901Z-001/Give_Back_My_Tower!/Scripts/transition_manager.gd
+++ Give_Back_My_Tower!-20241110T010901Z-001/Give_Back_My_Tower!/Scripts/transition_manager.gd
@@ -4,19 +4,10 @@ extends CanvasLayer
 var new_scene:= ""
 
 func fade_to_scene(newScene):
-	_remove_player_from_scene()
+	VariaveisGlobais.remove_player_from_scene()
 	new_scene = newScene
 	$AnimationPlayer.play("fade")
 
 func chang_scene():
-	_remove_player_from_scene()
+	VariaveisGlobais.remove_player_from_scene()
 	get_tree().change_scene_to_file(new_scene)
-
-"""
-This method prevents global player_instance to be cleared from memory 
-when changing scenes.
-"""
-func _remove_player_from_scene(): 
-	var player: PlayerBase = VariaveisGlobais.player_instance
-	if player != null and player.get_parent() != null:
-		player.get_parent().remove_child(player)

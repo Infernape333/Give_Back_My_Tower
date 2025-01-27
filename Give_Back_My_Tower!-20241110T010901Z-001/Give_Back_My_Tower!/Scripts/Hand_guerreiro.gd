102db8253ba7e6602d77ba014dcd49b2ea53b80a
Hélio Nogueira <heliofilhofhnlf@gmail.com>
2025-01-27 14:49:39 -0300

    .

--- Give_Back_My_Tower!-20241110T010901Z-001/Give_Back_My_Tower!/Scripts/Hand_guerreiro.gd
+++ Give_Back_My_Tower!-20241110T010901Z-001/Give_Back_My_Tower!/Scripts/Hand_guerreiro.gd
@@ -5,6 +5,10 @@ var is_in_initial_scene: bool = false
 
 @onready var attack = $"../PlayerAnm"
 
+var facing_right = true
+@onready var sword_area = $Area2D
+@onready var collision_polygon = $Area2D/CollisionPolygon2D
+
 func _ready():
 	if get_tree().current_scene.name == "Node2D":
 		is_in_initial_scene = true
@@ -12,18 +16,30 @@ func _ready():
 		is_in_initial_scene = false
 	timer.wait_time = VariaveisGlobais.atk_spd
 	
-#func animate(attack_direction: Vector2, direction: Vector2) -> void:
-	#if attack_direction.x > 0:
-		#staff.flip_v = false
-	#if attack_direction.x < 0:
-		#staff.flip_v = true
-	#look_at(direction)
+	collision_polygon.disabled = true
+	
+	
+func animate(attack_direction: Vector2, direction: Vector2) -> void:
+	# Ajusta o flip e posição da colisão (CollisionShape2D)
+	flip_collision_polygon(collision_polygon, attack_direction)
+		
+func flip_collision_polygon(collision_polygon: CollisionPolygon2D, attack_direction: Vector2) -> void:
+	var points = collision_polygon.polygon
+	
+	for i in range(points.size()):
+		points[i].x = abs(points[i].x) if attack_direction.x > 0 else -abs(points[i].x)
+	
+	collision_polygon.polygon = points
+
+
 
 func atk() -> void:
 	$"..".is_attacking = true
+	collision_polygon.disabled = false
 	attack.stop()
 	attack.play("attack")
 	await attack.animation_finished
+	collision_polygon.disabled = true
 	$"..".is_attacking = false
 
 func _on_timer_timeout():
